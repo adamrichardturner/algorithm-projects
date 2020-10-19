2c148b0e0fc40972a9710418e36bb1063ca03550
adamrichardturner <hello@adamrichardturner.dev>
2020-10-19 23:50:25 +0100

    initial upload
    Algorithm that produces a triangle shape with a variable base length

---
+++ triangle.js
@@ -0,0 +1,20 @@
+// Write a loop that makes seven calls to console.log
+// to output a triangle shape. You can use .length
+
+// triangle() takes a number (the length of the base of the output triangle)
+// it checks if baseLength is a number, if so, it loops, ending the loop
+// when the base length is reached. On each iteration of the loop it concatenates
+// bit to itself (the symbol #). Produces is an incrementally growing triangle 
+let triangle = (baseLength) => {
+    let bit = '#';
+    if(typeof baseLength === 'number') {
+        while (bit.length <= baseLength) {
+            console.log(bit);
+            bit = bit + '#';
+        }
+    } else {
+        console.log('You need to input the base as a number.')
+    }
+}
+
+triangle(48);
