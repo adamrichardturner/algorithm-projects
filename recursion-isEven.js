709dad759c738b1952c3cc8aee6db9f0b8099e6e
adamrichardturner <hello@adamrichardturner.dev>
2020-10-20 18:51:13 +0100

    commit recursive isEven function
    A recursive function that checks if a given, positive integer is even or not.

---
+++ recursion-isEven.js
@@ -0,0 +1,23 @@
+// Define a recursive function isEven that accepts
+// a single parameter (a positive, whole number) and returns
+// a boolean (true for even). 
+
+// Define it using the following rules: a. zero is even, b. one is odd
+// c. For any other number, N, its evenness is the same as N-2.
+
+const isEven = num => {
+    if(typeof num === 'number') {
+        if(num === 0) {
+            return true;
+        } else if(num === 1) {
+            return false;
+        } else if (num > 1) {
+            return isEven(num - 2);
+        } else if (num < 0) {
+            return 'Not a positive whole number';
+        }
+    }
+}
+
+console.log(isEven(50)); // false
+console.log(isEven(-1)); // 'Not a positive whole number'
